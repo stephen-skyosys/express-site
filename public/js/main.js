868d42b0330b98a150a00e7233234d91cf45bf98
lycanzzz <62604089+lycanzzz@users.noreply.github.com>
2021-01-23 17:35:42 +0530

    Routed messages to spreadsheet through an api, made changes to the footer

--- public/js/main.js
+++ public/js/main.js
@@ -219,3 +219,17 @@
 })(jQuery);
 
 
+function SubForm (){
+  $.ajax({
+    url:"https://api.apispreadsheets.com/data/7046/",
+    type:"post",
+    data:$("#myForm").serializeArray(),
+    success: function(){
+      alert("Form Data Submitted :)")
+    },
+    error: function(){
+      alert("There was an error :(")
+    }
+  });
+}
+
